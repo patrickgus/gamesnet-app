3b366234258e1b8ce61cd06e589a04c72c368093
Patrick Gustafson <patrickgus@charter.net>
2020-02-04 12:00:47 -0800

    create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Header extends Component {
+  renderLoginLink() {
+    return (
+      <div className="Header__not-logged-in">
+        <Link to="/register">Register</Link>
+        <Link to="/">Log in</Link>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <nav className="Header">
+        <h1>
+          <Link to="/">GamesNet</Link>
+        </h1>
+        {this.renderLoginLink()}
+      </nav>
+    );
+  }
+}
