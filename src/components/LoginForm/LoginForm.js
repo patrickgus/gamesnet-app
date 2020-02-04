90dea729513500520353762a92f3ad232beacb95
Patrick Gustafson <patrickgus@charter.net>
2020-02-04 12:00:59 -0800

    create LoginForm component

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+export default class LoginForm extends Component {
+  static defaultProps = {
+    onLoginSuccess: () => {}
+  };
+
+  state = { error: null };
+
+  render() {
+    const { error } = this.state;
+    return (
+      <form className="LoginForm">
+        <div role="alert">{error && <p className="red">{error}</p>}</div>
+        <div className="user_name">
+          <label htmlFor="LoginForm__username">Username</label>
+          <input type="text" name="username" id="LoginForm__username" />
+        </div>
+        <div className="password">
+          <label htmlFor="LoginForm__password">Password</label>
+          <input
+            name="password"
+            type="password"
+            id="LoginForm__password"
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    );
+  }
+}
