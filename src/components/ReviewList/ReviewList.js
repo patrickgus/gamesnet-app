ce38847356b6830ee05463c641b19daa4d20bbfb
Patrick Gustafson <patrickgus@charter.net>
2020-02-04 21:53:59 -0800

    create ReviewList and ListItem

---
+++ src/components/ReviewList/ReviewList.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import ReviewListItem from "../ReviewListItem/ReviewListItem";
+import { Section } from "../../components/Utils/Utils";
+import ReviewListContext from "../../contexts/ReviewListContext";
+
+export default class ReviewList extends Component {
+  static contextType = ReviewListContext;
+
+  componentDidMount() {
+    this.context.clearError()
+  }
+
+  renderReviews() {
+    const { reviewList = [] } = this.context;
+    return reviewList.map(review => (
+      <ReviewListItem key={review.id} review={review} />
+    ));
+  }
+
+  render() {
+    const { error } = this.context;
+    return (
+      <Section list className="ReviewList">
+        {error ? (
+          <p className="red">There was an error, try again</p>
+        ) : (
+          this.renderReviews()
+        )}
+      </Section>
+    );
+  }
+}
---
+++ src/components/ReviewListItem/ReviewListItem.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Section } from "../../components/Utils/Utils";
+
+export default class ReviewListItem extends Component {
+  render() {
+    const { review } = this.props;
+    return (
+      <Section className="ReviewListItem">
+        <h4>{review.title}</h4>
+        <p>Rating: {review.rating}</p>
+        <div className="reviewInfo">
+          <p>{review.description}</p>
+        </div>
+      </Section>
+    );
+  }
+}
