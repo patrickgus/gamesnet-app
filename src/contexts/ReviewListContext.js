8a8b78f9e3e9be0079cf20bb66cdd39ac66ecc01
Patrick Gustafson <patrickgus@charter.net>
2020-02-04 21:54:12 -0800

    create review context

---
+++ src/contexts/ReviewListContext.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import reviewList from "../review-store";
+
+const ReviewListContext = React.createContext({
+  reviewList,
+  error: null,
+  setError: () => {},
+  clearError: () => {},
+  setReviewList: () => {}
+});
+
+export default ReviewListContext;
+
+export class ReviewListProvider extends Component {
+  state = {
+    reviewList,
+    error: null
+  };
+
+  setReviewList = reviewList => {
+    this.setState({ reviewList });
+  };
+
+  setError = error => {
+    console.error(error);
+    this.setState({ error });
+  };
+
+  clearError = () => {
+    this.setState({ error: null });
+  };
+
+  render() {
+    const value = {
+      reviewList: this.state.reviewList,
+      error: this.state.error,
+      setError: this.setError,
+      clearError: this.clearError,
+      setReviewList: this.setReviewList
+    };
+    return (
+      <ReviewListContext.Provider value={value}>
+        {this.props.children}
+      </ReviewListContext.Provider>
+    );
+  }
+}
