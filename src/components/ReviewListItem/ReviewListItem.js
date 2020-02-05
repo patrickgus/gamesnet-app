5bb9dcdb52c37142c6b8cd00c10c5ad01ad8e681
Patrick Gustafson <patrickgus@charter.net>
2020-02-05 07:39:02 -0800

    add styles to GameReviewPage

--- src/components/ReviewListItem/ReviewListItem.js
+++ src/components/ReviewListItem/ReviewListItem.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import { Section } from "../../components/Utils/Utils";
+import "./ReviewListItem.css";
 
 export default class ReviewListItem extends Component {
   render() {
     const { review } = this.props;
     return (
       <Section className="ReviewListItem">
-        <h4>{review.title}</h4>
+        <h4>"{review.title}"</h4>
         <p>Rating: {review.rating}</p>
         <div className="reviewInfo">
           <p>{review.description}</p>
