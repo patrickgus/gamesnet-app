53aadfff4d795c7488db1b41b2503d7fb70ffbd0
Patrick Gustafson <patrickgus@charter.net>
2020-02-04 20:09:16 -0800

    add providers

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App/App";
 import "./index.css";
+import { GameListProvider } from "./contexts/GameListContext";
+import { GameProvider } from "./contexts/GameContext";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <GameListProvider>
+      <GameProvider>
+        <App />
+      </GameProvider>
+    </GameListProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
