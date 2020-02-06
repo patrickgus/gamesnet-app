265bb3453f598d385bfc0d1f78d65aae20065987
Patrick Gustafson <patrickgus@charter.net>
2020-02-05 19:13:10 -0800

    refactor to functional component

--- src/components/GameListItem/GameListItem.js
+++ src/components/GameListItem/GameListItem.js
@@ -1,27 +1,26 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import { Section } from "../../components/Utils/Utils";
 import "./GameListItem.css";
 
-export default class GameListItem extends Component {
-  render() {
-    const { game } = this.props;
-    return (
-      <Section className="GameListItem">
-        <header className="GameListItem__header">
-          <Link to={`/review/${game.id}`} className="GameListItem__link">
-            <h2 className="GameListItem__heading">{game.title}</h2>
-          </Link>
-          <img src={game.cover} alt={`Game cover art for ${game.title}`} />
-          <h4>Avg Rating: {game.avg_rating}</h4>
-        </header>
-        <div className="gameInfo">
-          <h4>Description</h4>
-          <p>{game.description}</p>
-          <p>Rated: {game.rated}</p>
-          <p>Platforms: {game.platforms}</p>
-        </div>
-      </Section>
-    );
-  }
+export default function GameListItem(props) {
+  const game = props.game;
+  
+  return (
+    <Section className="GameListItem">
+      <header className="GameListItem__header">
+        <Link to={`/review/${game.id}`} className="GameListItem__link">
+          <h2 className="GameListItem__heading">{game.title}</h2>
+        </Link>
+        <img src={game.cover} alt={`Game cover art for ${game.title}`} />
+        <h4>Avg Rating: {game.avg_rating}</h4>
+      </header>
+      <div className="gameInfo">
+        <h4>Description</h4>
+        <p>{game.description}</p>
+        <p>Rated: {game.rated}</p>
+        <p>Platforms: {game.platforms}</p>
+      </div>
+    </Section>
+  );
 }
--- src/components/ReviewListItem/ReviewListItem.js
+++ src/components/ReviewListItem/ReviewListItem.js
@@ -2,17 +2,16 @@ import React, { Component } from "react";
 import { Section } from "../../components/Utils/Utils";
 import "./ReviewListItem.css";
 
-export default class ReviewListItem extends Component {
-  render() {
-    const { review } = this.props;
-    return (
-      <Section className="ReviewListItem">
-        <h4>"{review.title}"</h4>
-        <p>Rating: {review.rating}</p>
-        <div className="reviewInfo">
-          <p>{review.description}</p>
-        </div>
-      </Section>
-    );
-  }
+export default function ReviewListItem(props) {
+  const review = props.review;
+
+  return (
+    <Section className="ReviewListItem">
+      <h4>"{review.title}"</h4>
+      <p>Rating: {review.rating}</p>
+      <div className="reviewInfo">
+        <p>{review.description}</p>
+      </div>
+    </Section>
+  );
 }
