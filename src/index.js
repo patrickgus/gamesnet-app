1933fd7cab8e14c51755ca13c3d4ed773563b7ac
Patrick Gustafson <patrickgus@charter.net>
2020-02-08 13:00:16 -0800

    import Game and GameList providers

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
+import { GameListProvider } from "./contexts/GameListContext";
+import { GameProvider } from "./contexts/GameContext";
 import App from "./components/App/App";
 import "./index.css";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <GameListProvider>
+      <GameProvider>
+        <App />
+      </GameProvider>
+    </GameListProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
