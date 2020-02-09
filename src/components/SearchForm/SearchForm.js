876fdb55044da9ea742fefb3b210843b111ae26a
Patrick Gustafson <patrickgus@charter.net>
2020-02-09 16:20:12 +0000

    add handler methods for inputs

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,15 +1,78 @@
 import React, { Component } from "react";
+// import GameListContext from "../../contexts/GameListContext";
 import "./SearchForm.css";
 
 export default class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filteredList: [],
+      searchTerm: "",
+      sort: ""
+    };
+  }
+
+  // static contextType = GameListContext;
+
+  handleSearch = e => {
+    this.setState({
+      searchTerm: e.target.value
+    });
+  };
+
+  handleSort = e => {
+    this.setState({
+      sort: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const gameList = this.props.games;
+    const { sort } = this.state;
+    let filteredList = [];
+
+    if (this.state.searchTerm !== "") {
+      filteredList = gameList.filter(game => {
+        const title = game.props.game.title.toLowerCase();
+        const filter = this.state.searchTerm.toLowerCase();
+        return title.includes(filter);
+      });
+    } else {
+      filteredList = this.props.games;
+    }
+
+    if (sort) {
+      filteredList.sort((a, b) => {
+        return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      });
+    }
+
+    this.setState({
+      filteredList
+    });
+    console.log(filteredList);
+  };
+
   render() {
     return (
-      <form className="searchBarNav">
+      <form className="searchBarNav" onSubmit={this.handleSubmit}>
         <label htmlFor="search">Search: </label>
-        <input type="text" id="search" name="search" onChange={this.handleSearch} />
+        <input
+          type="text"
+          id="search"
+          name="search"
+          value={this.state.searchTerm}
+          onChange={this.handleSearch}
+        />
 
         <label htmlFor="sort">Sort: </label>
-        <select id="sort" name="sort" onChange={this.handleSort}>
+        <select
+          id="sort"
+          name="sort"
+          value={this.state.sort}
+          onChange={this.handleSort}
+        >
           <option value="title">Title</option>
           <option value="rating">Rating</option>
         </select>
