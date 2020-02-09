5fa58ea3ff55866edf604fb2b67c061e6cc64a7c
Patrick Gustafson <patrickgus@charter.net>
2020-02-08 20:35:05 -0800

    implement logout function

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import TokenService from "../../services/token-service";
+import IdleService from "../../services/idle-service";
 import { Hyph } from "../Utils/Utils";
 import "./Header.css";
 
 export default class Header extends Component {
+  handleLogoutClick = () => {
+    TokenService.clearAuthToken();
+    /* when logging out, clear the callbacks to the refresh api and idle auto logout */
+    TokenService.clearCallbackBeforeExpiry();
+    IdleService.unRegisterIdleResets();
+  };
+
+  renderLogoutLink() {
+    return (
+      <div className="Header__logged-in">
+        <Link onClick={this.handleLogoutClick} to="/">
+          Logout
+        </Link>
+      </div>
+    );
+  }
+
   renderLoginLink() {
     return (
       <div className="Header__not-logged-in">
-        <Link to="/register">Register</Link>
-        <Hyph />
         <Link to="/">Log in</Link>
+        <Hyph />
+        <Link to="/register">Register</Link>
       </div>
     );
   }
@@ -20,7 +39,9 @@ export default class Header extends Component {
         <h1>
           <Link to="/">GamesNet</Link>
         </h1>
-        {this.renderLoginLink()}
+        {TokenService.hasAuthToken()
+          ? this.renderLogoutLink()
+          : this.renderLoginLink()}
       </nav>
     );
   }
