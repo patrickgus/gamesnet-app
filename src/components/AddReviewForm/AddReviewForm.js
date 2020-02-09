ca79669b43a3082769b56b754e56caf8f91f7535
Patrick Gustafson <patrickgus@charter.net>
2020-02-08 18:05:02 -0800

    implement handleSubmit to post review via api call

--- src/components/AddReviewForm/AddReviewForm.js
+++ src/components/AddReviewForm/AddReviewForm.js
@@ -1,16 +1,64 @@
 import React, { Component } from "react";
-import { Required, Textarea, Button } from "../../components/Utils/Utils";
+import { withRouter } from "react-router-dom";
+import GameContext from "../../contexts/GameContext";
+import GameApiService from "../../services/game-api-service";
+import { Input, Required, Textarea, Button } from "../../components/Utils/Utils";
 import "./AddReviewForm.css";
 
-export default class AddReviewForm extends Component {
+class AddReviewForm extends Component {
+  static defaultProps = {
+    match: { params: {} }
+  };
+
+  static contextType = GameContext;
+
+  handleSumbit = e => {
+    e.preventDefault();
+    const { gameId } = this.props.match.params;
+    const { game } = this.context;
+    const { title, rating, review } = e.target;
+
+    GameApiService.postGame(
+      game.id,
+      title.value,
+      Number(rating.value),
+      review.value
+    )
+      .then(this.context.addGame)
+      .then(() => {
+        title.value = "";
+        rating.value = "";
+        review.value = "";
+        this.props.history.push(`/review/${gameId}`);
+      })
+      .catch(this.context.setError);
+  };
+
+  handleCancel = e => {
+    e.preventDefault();
+    const { gameId } = this.props.match.params;
+    this.props.history.push(`/review/${gameId}`);
+  };
+
   render() {
     return (
       <form className="AddReviewForm" onSubmit={this.handleSumbit}>
+        <div className="review_title">
+          <label htmlFor="AddReviewForm__review_title">
+            Review title <Required />
+          </label>
+          <Input
+            name="title"
+            type="text"
+            required
+            id="AddReviewForm__review_title"
+          />
+        </div>
         <div className="game_rating">
           <label htmlFor="AddReviewForm__game_rating">
             Rating <Required />
           </label>
-          <select name="game_rating" required id="AddReviewForm__game_rating">
+          <select name="rating" required id="AddReviewForm__game_rating">
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -30,15 +78,17 @@ export default class AddReviewForm extends Component {
           <Textarea
             required
             aria-label="Write your review..."
-            name="game_review"
+            name="review"
             id="AddReviewForm__game_review"
             rows="15"
             placeholder="Write your review..."
           />
         </div>
         <Button type="submit">Add Review</Button>
-        <Button>Cancel</Button>
+        <Button onClick={this.handleCancel}>Cancel</Button>
       </form>
     );
   }
 }
+
+export default withRouter(AddReviewForm);
