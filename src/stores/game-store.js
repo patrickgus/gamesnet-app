282bb82d37d7fb7a420ca85420a20dcaaca56520
Patrick Gustafson <patrickgus@charter.net>
2020-02-10 13:23:29 -0800

    add store for test purposes

---
+++ src/stores/game-store.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    id: 1,
+    title: "First test game",
+    cover: "",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent aliquet, odio vitae imperdiet finibus, purus nisi tincidunt lorem, feugiat bibendum mi mi sit amet urna. Donec a elementum nunc, sed dapibus augue. Duis volutpat at nisl eget varius. Nam vitae libero auctor, lacinia quam ac, fermentum dolor. Donec arcu neque, faucibus eu porta nec, laoreet sit amet eros.",
+    rated: "M",
+    platforms: "Playstaion",
+    date_added: "2029-01-22T16:28:32.615Z",
+    avg_rating: 9
+  },
+  {
+    id: 2,
+    title: "Second test game",
+    cover: "",
+    description:
+      "Sed pharetra, ante vel aliquet viverra, augue nunc malesuada dui, vel sollicitudin enim sem at urna. Integer pharetra sodales dolor, id accumsan purus aliquam sed. Donec at magna convallis, varius turpis in, elementum eros. Aliquam erat volutpat. Ut at leo eu ligula fermentum fringilla malesuada ut nisl.",
+    rated: "T",
+    platforms: "Playstation, Xbox, PC, Mobile",
+    date_added: "2029-01-22T16:28:32.615Z",
+    avg_rating: 7
+  },
+  {
+    id: 3,
+    title: "Third test game",
+    cover: "",
+    description:
+      "Mauris erat justo, facilisis eget lacus convallis, feugiat luctus massa. Nullam molestie ullamcorper nisl sed posuere. Aliquam erat volutpat. Morbi sed suscipit neque, eu fermentum dolor. Vivamus luctus, eros eu finibus auctor, arcu erat pretium sem, ut tempor mi quam vitae dolor. Mauris feugiat mi in imperdiet commodo.",
+    rated: "M",
+    platforms: "Xbox, Windows",
+    date_added: "2029-01-22T16:28:32.615Z",
+    avg_rating: 8
+  }
+];
