4795e7c831539634cd01522d5ba85b252789bdd3
Patrick Gustafson <patrickgus@charter.net>
2020-02-10 13:23:49 -0800

    wrap component iwith BrowserRouter

--- src/components/SearchForm/SearchForm.test.js
+++ src/components/SearchForm/SearchForm.test.js
@@ -1,17 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import renderer from "react-test-renderer";
 import SearchForm from "./SearchForm";
 
 describe("SearchForm component", () => {
   it("renders without crashing", () => {
     const div = document.createElement("div");
-    ReactDOM.render(<SearchForm />, div);
+    ReactDOM.render(
+      <BrowserRouter>
+        <SearchForm />
+      </BrowserRouter>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it("renders the form as expected", () => {
-    const tree = renderer.create(<SearchForm />).toJSON();
+    const tree = renderer
+      .create(
+        <BrowserRouter>
+          <SearchForm />
+        </BrowserRouter>
+      )
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
