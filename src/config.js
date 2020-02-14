0665ac5faa7049ed79664914d4e11674e654c17d
Patrick Gustafson <patrickgus@charter.net>
2020-02-14 13:17:51 -0800

    change API url to heroku url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   API_BASE_URL:
     process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:8000/api",
+    "https://limitless-fortress-68165.herokuapp.com/api",
   TOKEN_KEY: "gamesnet-client-auth-token"
 };
